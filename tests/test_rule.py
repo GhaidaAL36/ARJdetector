75b604ead50e5b353235a0398c2e19c31017806f
GhaidaAl36 <ghaida3638@gmail.com>
2026-07-31 00:57:20 +0300

    t-12: get pattern test

---
+++ tests/test_rule.py
@@ -0,0 +1,31 @@
+from app.engine.rule import get_pattern
+
+
+def test_get_pattern_faael():
+    patterns = get_pattern("غاضب")
+
+    assert "1ا23" in patterns
+
+
+def test_get_pattern_faeel():
+    patterns = get_pattern("كبير")
+
+    assert "12ي3" in patterns
+
+
+def test_non_flagged_pattern():
+    patterns = get_pattern("مثلث")
+
+    assert "م123" in patterns
+    assert "1ا23" not in patterns
+    assert "12ي3" not in patterns
+    
+def test_get_pattern_unrecognized_word():
+    patterns = get_pattern("test")
+    
+    assert 'FOREIGN' in patterns 
+    
+def test_get_pattern_gibberish_word():
+    patterns = get_pattern("هيسباكخب")
+    
+    assert [] == patterns 
