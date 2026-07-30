e17aa5268ebe27c122a6f6b7173c53afdd9668fa
GhaidaAl36 <ghaida3638@gmail.com>
2026-07-31 01:22:09 +0300

    t-12: matches_flagged_pattern test

--- tests/test_rule.py
+++ tests/test_rule.py
@@ -1,5 +1,6 @@
-from app.engine.rule import get_pattern
+from app.engine.rule import get_pattern, matches_flagged_pattern
 
+""" get_pattern """
 
 def test_get_pattern_faael():
     patterns = get_pattern("غاضب")
@@ -29,3 +30,41 @@ def test_get_pattern_gibberish_word():
     patterns = get_pattern("هيسباكخب")
     
     assert [] == patterns 
+    
+""" matches_flagged_pattern """
+    
+def test_all_patterns_are_flagged():
+    rules = {
+        "flagged_patterns": ["1ا23", "12ي3"]
+    }
+
+    patterns = ["1ا23", "12ي3"]
+
+    assert matches_flagged_pattern(rules, patterns) is True
+    
+def test_one_pattern_is_flagged():
+    rules = {
+        "flagged_patterns": ["1ا23", "12ي3"]
+    }
+
+    patterns = ["م123", "1ا23"]
+
+    assert matches_flagged_pattern(rules, patterns) is True
+    
+def test_no_pattern_is_flagged():
+    rules = {
+        "flagged_patterns": ["1ا23", "12ي3"]
+    }
+
+    patterns = ["م123"]
+
+    assert matches_flagged_pattern(rules, patterns) is False
+    
+def test_unrecognized_word_pattern():
+    rules = {
+            "flagged_patterns": ["1ا23", "12ي3"]
+        }
+    
+    pattern = []
+    
+    assert matches_flagged_pattern(rules, pattern) is False
