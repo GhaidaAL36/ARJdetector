d223f2da36cf33a47787bc2012b7d36f4a20ee4d
GhaidaAl36 <ghaida3638@gmail.com>
2026-08-01 10:48:15 +0300

    t-9: build get_suggestion

--- app/engine/rule.py
+++ app/engine/rule.py
@@ -20,3 +20,34 @@ def matches_nisba_pattern(pattern):
     has_flagged_pattern = any(p.endswith("ي") for p in pattern)
     return has_flagged_pattern
 
+SPECIAL_CASES = {
+    "كبير": "جداً",
+    "ناجح": "بنجاح",
+}
+
+FIXED_EXPLANATION = "حشو أسلوبي"
+
+def to_accusative_tanween(word):
+    
+    if word.endswith("ة"):
+        return word[:-1] + "ةً"
+        
+    elif word.endswith("اء"):
+        return word[:-1] + "ءً"
+        
+    elif word.endswith("ء"):
+        return word[:-1] + "ئاً"
+        
+    elif word.endswith("ي"):
+        return word[:-1] + "ياً"
+    
+    else:
+        return word + "اً"
+    
+def get_suggestion(word):
+    if word in SPECIAL_CASES:
+        return SPECIAL_CASES[word]
+    return to_accusative_tanween(word)
+
+def get_explanation():
+    return FIXED_EXPLANATION
