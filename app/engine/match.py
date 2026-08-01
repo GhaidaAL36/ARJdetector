fa3e710ca659ab1a16cce308d2951b3c21a8fd45
GhaidaAl36 <ghaida3638@gmail.com>
2026-08-01 13:31:38 +0300

    handle special_cases

--- app/engine/match.py
+++ app/engine/match.py
@@ -1,12 +1,13 @@
 from app.engine.rule import get_explanation, get_suggestion
 
 
-def build_match(trigger_word, word):
+def build_match(trigger_word, word, special_cases):
+    
     return {
         "flagged": True,
         "flagged_phrase": trigger_word + " " + word,
         "explanation": get_explanation(),
-        "suggestion": get_suggestion(word),
+        "suggestion": get_suggestion(word, special_cases),
     }
 
 
