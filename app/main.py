9f6422c5ae74f9942181aad856852ead4fba2bbc
GhaidaAl36 <ghaida3638@gmail.com>
2026-08-02 16:48:27 +0300

    t-10: analyze ep

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.engine.rule_engine import analyze
+from app.schemas import AnalyzeRequest
+from app.config import rules_path,suggestions_path
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, FastAPI!"}
+@app.post("/analyze")
+def analyze_endpoint(request: AnalyzeRequest):
+    result = analyze(rules_path, suggestions_path, request.text)
+    return result
