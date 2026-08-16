c8c48b21feaddeed96efb2caba1970ec3cb1aa3f
GhaidaAl36 <ghaida3638@gmail.com>
2026-08-16 20:06:11 +0300

    spilt file to another files

--- app/engine/rule.py
+++ app/engine/rule.py
@@ -1,34 +1,3 @@
-from camel_tools.morphology.database import MorphologyDB
-from camel_tools.morphology.analyzer import Analyzer
-from camel_tools.utils.dediac import dediac_ar
-from camel_tools.disambig.mle import MLEDisambiguator
-
-_mle = None
-
-
-def _get_mle():
-    global _mle
-    if _mle is None:
-        _mle = MLEDisambiguator.pretrained()
-    return _mle
-
-
-def get_pos_and_pattern_in_context(tokens):
-    mle = _get_mle()
-    words = [word for _, word in tokens]
-    disambiguated = mle.disambiguate(words)
-
-    results = []
-    for entry in disambiguated:
-        top_analysis = entry.analyses[0].analysis
-        pos = top_analysis.get("pos", "")
-        pattern = dediac_ar(top_analysis.get("pattern", ""))
-        lex = dediac_ar(top_analysis.get("lex", ""))
-        results.append({"pos": pos, "pattern": pattern, "lex": lex})
-
-    return results
-
-
 def matches_flagged_pattern(rules, pos_pattern_info):
     return (
         pos_pattern_info["pattern"] in rules["flagged_patterns"]
